6112e474b6c182a6db7adb279fd2305d16dd861c
ManzzyGacor <manzz9293@gmail.com>
2025-10-24 19:35:14 +0800

    Create Information model with Mongoose schema

---
+++ api/models/Information.js
@@ -0,0 +1,11 @@
+// api/models/Information.js
+const mongoose = require('mongoose');
+
+const InformationSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+    createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('Information', InformationSchema);
