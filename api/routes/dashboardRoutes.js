04154da4c30c81e7120492cc4b32bd45f3ae6527
ManzzyGacor <manzz9293@gmail.com>
2025-10-25 18:40:36 +0800

    Update dashboardRoutes.js

--- api/routes/dashboardRoutes.js
+++ api/routes/dashboardRoutes.js
@@ -61,7 +61,8 @@ router.post('/purchase/pterodactyl', protect, async (req, res) => {
     const DOCKER_IMAGE_NODEJS = 'ghcr.io/parkervcp/yolks:nodejs_24'; // Ganti versi Node jika perlu
     const STARTUP_NODEJS = 'if [[ -d .git ]] && [[ {{AUTO_UPDATE}} == "1" ]]; then git pull; fi; if [[ ! -z ${NODE_PACKAGES} ]]; then /usr/local/bin/npm install ${NODE_PACKAGES}; fi; if [[ ! -z ${UNNODE_PACKAGES} ]]; then /usr/local/bin/npm uninstall ${UNNODE_PACKAGES}; fi; if [ -f /home/container/package.json ]; then /usr/local/bin/npm install; fi;  if [[ ! -z ${CUSTOM_ENVIRONMENT_VARIABLES} ]]; then      vars=$(echo ${CUSTOM_ENVIRONMENT_VARIABLES} | tr ";" "\n");      for line in $vars;     do export $line;     done fi;  /usr/local/bin/${CMD_RUN};'; // Ganti index.js jika nama file utama beda
     // HAPUS: const ENVIRONMENT_WHATSAPP = { ... };
-    const FEATURE_LIMITS_DEFAULT = { databases: 0, backups: 1, allocations: 1 }; // Sesuaikan
+   const ENVIRONMENT_DEFAULT = {};
+  const FEATURE_LIMITS_DEFAULT = { databases: 0, backups: 1, allocations: 1 }; // Sesuaikan
     const IO_DEFAULT = 500;
 
     const SERVER_PACKAGES = {
